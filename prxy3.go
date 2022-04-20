f1737fae8a103c79a16770c4130eec7604c1fcd5
oli <oli.wurster@gmail.com>
2022-04-20 14:58:33 +0200

    own method for response processing

--- prxy3.go
+++ prxy3.go
@@ -15,8 +15,8 @@ type transport struct {
 	http.RoundTripper
 }
 
-func printRequestInfo(request *http.Request) (*http.Request, error) {
-	fmt.Println("******* request *******")
+func processRequest(request *http.Request) (*http.Request, error) {
+	fmt.Print("\n\nREQUEST\n")
 	fmt.Println("url: ", request.URL)
 	fmt.Println("method: ", request.Method)
 
@@ -40,38 +40,43 @@ func printRequestInfo(request *http.Request) (*http.Request, error) {
 	return request, nil
 }
 
-func (t *transport) RoundTrip(req *http.Request) (resp *http.Response, err error) {
-	req, err = printRequestInfo(req)
+func processResponse(response *http.Response) (*http.Response, error) {
+	respB, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+	err = response.Body.Close()
 	if err != nil {
 		return nil, err
 	}
 
-	resp, err = t.RoundTripper.RoundTrip(req)
-
-	if err != nil {
-		return nil, err
-	}
-	respB, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	err = resp.Body.Close()
-	if err != nil {
-		return nil, err
-	}
-
-	fmt.Println("******* response *******")
-	fmt.Println("status code: ", resp.StatusCode)
+	fmt.Print("\n\nRESPONSE\n")
+	fmt.Println("status code: ", response.StatusCode)
 	fmt.Println("begin response body")
 	fmt.Println(string(respB))
 	fmt.Println("end response body")
 
 	body := ioutil.NopCloser(bytes.NewReader(respB))
 
-	resp.Body = body
-	resp.ContentLength = int64(len(respB))
-	resp.Header.Set("Content-Length", strconv.Itoa(len(respB)))
-	return resp, nil
+	response.Body = body
+	response.ContentLength = int64(len(respB))
+	response.Header.Set("Content-Length", strconv.Itoa(len(respB)))
+	return response, nil
+}
+
+func (t *transport) RoundTrip(req *http.Request) (resp *http.Response, err error) {
+	req, err = processRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err = t.RoundTripper.RoundTrip(req)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err = processResponse(resp)
+	return resp, err
 }
 
 func main() {
